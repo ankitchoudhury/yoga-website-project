31ac672f962cb0d6b040e49d2aabceba89921a65
ankitchoudhury <ankitchoudhury.dev@gmail.com>
2026-03-16 23:52:09 +0530

    user prefrence of website look saved in local storage

--- scriipt.js
+++ scriipt.js
@@ -18,18 +18,29 @@ function toogleMenu() {
     isMenuOpen = !isMenuOpen;
     menuEle.style.display = isMenuOpen ? "flex" : "none"
 }
+// js for darkmode
 
 const darkModeEle = document.getElementById("darkmode");
 
 const iconEle = document.getElementById("darkmode-icon");
 
+const savedTheme = localStorage.getItem("theme");
+
+if(savedTheme === "dark"){
+    document.body.classList.add("darkmode")
+    iconEle.src = "img/sun.png"
+    
+}
+
 darkModeEle.addEventListener("click", () => {
     document.body.classList.toggle("darkmode");
-    if(document.body.classList.contains("darkmode")){
+    if (document.body.classList.contains("darkmode")) {
         iconEle.src = "img/sun.png"
+        localStorage.setItem("theme", "dark")
     }
-    else{
+    else {
         iconEle.src = "img/moon-icon.png"
+        localStorage.setItem("theme", "light")
     }
 });
 
